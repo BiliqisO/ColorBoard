bc4355bb2d69fb047a6c42cde0f97f381328d908
Biliqis Onikoyi <105673949+BiliqisO@users.noreply.github.com>
2024-05-10 13:44:49 +0100

    Update main.js
    update window.web3 changes

--- Frontend/main.js
+++ Frontend/main.js
@@ -100,7 +100,7 @@ function revealColor(e) {
 }
 async function reveal() {
   window.ethereum = await new Web3(window.ethereum);
-  window.contract = await new window.web3.eth.Contract(
+  window.contract = await new window.ethereum.Contract(
     contractABI,
     contractAddress
   );
